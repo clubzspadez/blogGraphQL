2aa3a9b0890b3444b5c97d86ee4daa61a16b1a76
clubzspadez <clubzspadez@gmail.com>
2019-02-22 17:28:53 -0500

    Working on adding graphql server

---
+++ graphql/src/index.js
@@ -0,0 +1,50 @@
+import { GraphQLServer} from "graphql-yoga";
+
+const typeDefs = `
+    type Query {
+        name: String!
+        age: Int!
+        location:String!
+        bio: String!
+    }
+`
+
+const user = ( { name, age, location, bio }) => {
+    return {
+        name,
+        age,
+        location,
+        bio
+    }
+};
+
+const user = {
+    name: 'Jonathan',
+    age: 26,
+    location: 'Burlington ',
+    bio: 'This is your typical bio!'
+}
+
+
+
+const resolvers = ({ name, age, location, bio }) => {
+    return {
+        Query: {
+            name: () => {
+                return ` My name is ${name}`
+            },
+            age: () => `My age is ${age}`,
+            location: () => `My location is ${location}`,
+            bio: () => `${bio}`
+        }
+    }
+}
+
+resolvers(user);
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start( () => console.log(`Server is running`))
