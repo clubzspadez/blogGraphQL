b94f4ee6608fb5d0d8e8b7e983baad4d794e55e8
clubzspadez <clubzspadez@gmail.com>
2019-03-01 15:18:42 -0500

    learning custom types

--- src/index.js
+++ src/index.js
@@ -2,48 +2,116 @@ import {
     GraphQLServer
 } from "graphql-yoga";
 
+//Firs example type definition
+// const typeDefs = `
+//     type Query {
+//         name: String!
+//         age: Int!
+//         location:String!
+//         bio: String!
+//     }
+// `
+
+// second example type definition with all Scalar types
+// const typeDefs = `
+//     type Query {
+//         id: ID!
+//         name: String!
+//         age: Int!
+//         location:String!
+//         alive: Boolean!
+//         salary: Float!
+//     }
+// `;
+
+// third example type definition with all Custom types
 const typeDefs = `
     type Query {
+        me: User
+    }
+    
+    type User {
+        id: ID!
         name: String!
         age: Int!
         location:String!
-        bio: String!
+        alive: Boolean!
+        salary: Float!
     }
-`
+`;
 
 
-const user = {
+
+// const userInformation = {
+//     name: 'Jonathan',
+//     age: 26,
+//     location: 'Burlington ',
+//     bio: 'This is your typical bio!'
+// }
+
+const userInformation2 = {
+    id: 'NKCQJDMUIH1231',
     name: 'Jonathan',
     age: 26,
     location: 'Burlington ',
-    bio: 'This is your typical bio!'
-}
+    bio: 'This is your typical bio!',
+    alive: true,
+    salary: 21120123.223
+};
 
 
 
-const resolvers = ({
-    name,
-    age,
-    location,
-    bio
-}) => {
+// const resolverSetup = ({
+//     id,
+//     name,
+//     age,
+//     location,
+//     alive,
+//     salary
+// }) => {
+//     return {
+//         Query: {
+//             id: () =>  id,
+//             name: () => ` My name is ${name}`,
+//             age: () => age,
+//             location: () => `My location is ${location}`,
+//             alive: () => alive,
+//             salary: () => salary
+//         }
+//     }
+// };
+
+//resolver with customer type being returned for me query
+const resolverSetup = ({
+                           id,
+                           name,
+                           age,
+                           location,
+                           alive,
+                           salary
+                       }) => {
     return {
         Query: {
-            name: () => {
-                return ` My name is ${name}`
-            },
-            age: () => `My age is ${age}`,
-            location: () => `My location is ${location}`,
-            bio: () => `${bio}`
+            me: () => {
+                return {
+                    id,
+                    name,
+                    age,
+                    location,
+                    alive,
+                    salary
+                }
+            }
         }
     }
-}
+};
 
-resolvers(user);
+// const resolvers = resolverSetup(userInformation);
+const resolvers = resolverSetup(userInformation2);
 
 const server = new GraphQLServer({
     typeDefs,
     resolvers
-})
+});
 
-server.start(() => console.log(`Server is running`))
+server.start(() => console.log(`Server is running`));
